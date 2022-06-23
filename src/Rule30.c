2a8826a5a4bb9ddcafac31606d63c9eb27bfcc2c
chloe0b0 <ChloeZeiter@gmail.com>
2022-06-23 04:35:35 -0700

    32 bit state:

--- src/Rule30.c
+++ src/Rule30.c
@@ -5,28 +5,32 @@
 #include <time.h>
 #include <stdbool.h>
 
-// For abitrary state sizes, will need an array of N booleans, this means 8 bits per cell (assuming each is a boolean)
+// For abitrary state sizes, will need an array of N booleans, this means 32 bits per cell (assuming each is a boolean)
 // If we instead use an usigned long long, we can get 1 bit per cell and use bitwise operations to update the state
 
 // Helper macros
 #define N_TH(x, n)  x >> n & 1 // Get the N-th bit
-#define SET(x, n)   x |= (1ULL << n)
+#define SET(x, n)   x |= (1 << n)
 
-typedef uint64_t State;
+// Constants
+#define EPOCHS  500
+#define BITS    15
 
-State Iterate(State state){
+typedef uint32_t State;
+
+static inline State Iterate(State state){
     State n_state = (state >> 1) ^ (state | (state << 1));
    
     // Hackish trick to prevent overflow of active cells
-    if (N_TH(state, 63)){
-        n_state ^= 1ULL << 63;
+    if (N_TH(state, 31)){
+        n_state ^= (1UL << 31);
     }
     return n_state;
 }
 
 // Yield the center cell's current state
-uint64_t Yield(State state){
-    return N_TH(state, 31); 
+static inline uint32_t Yield(State state){
+    return N_TH(state, 32/2); 
 }
 
 uint64_t Generate_64(State rand, size_t bits){
@@ -41,19 +45,25 @@ uint64_t Generate_64(State rand, size_t bits){
 }
 
 void PrintState(State state){
-    for (int i = 64; i--;){
-        const char cell = N_TH(state, i) ? '1' : ' ';
+    for (int i = 32; i--;){
+        const char cell = N_TH(state, i) ? '1' : '0';
         printf("%c", cell);
     }
     printf("\n");
 }
 
 int main(void){
-    assert(sizeof(State) == 8);
-    State rand = (uint64_t)time(NULL) * time(NULL) * time(NULL);
+    assert(sizeof(State) == 4);
+    State rand = (uint32_t)time(NULL) * time(NULL) * time(NULL);
 
-    for (int i = 0; i < 250; ++i){
+    clock_t t;
+    t = clock();
+    for (int i = EPOCHS; --i;){
         rand = Iterate(rand);
-        printf("%llu, ", Generate_64(rand, 15));
+        uint64_t x = Generate_64(rand, BITS);
+        printf("%lu \n", x);
     }
+    t = clock() - t;
+    double t_ = ((double)t/CLOCKS_PER_SEC);
+    printf("Time taken to generate %d random %d-bit pseudo-random numbers: %f seconds\n", EPOCHS, BITS, t_);
 }
