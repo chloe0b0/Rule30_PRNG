3461ce4fae53389f46dc2de2bf868b301fe7ee5f
chloe0b0 <ChloeZeiter@gmail.com>
2022-06-24 00:24:39 -0700

    Switch to 64 bit state

--- src/Rule30.c
+++ src/Rule30.c
@@ -5,7 +5,7 @@
 #include <time.h>
 #include <stdbool.h>
 
-// For abitrary state sizes, will need an array of N booleans, this means 32 bits per cell (assuming each is a boolean)
+// For abitrary state sizes, will need an array of N booleans, this means 64 bits per cell (assuming each is a boolean)
 // If we instead use an usigned long long, we can get 1 bit per cell and use bitwise operations to update the state
 
 // Helper macros
@@ -13,15 +13,15 @@
 #define SET(x, n)   x |= (1 << n)
 
 // Constants
-#define EPOCHS  5000000
-#define BITS    32
+#define EPOCHS  500
+#define BITS    1
 
-typedef uint32_t State;
+typedef uint64_t State;
 
 static inline void Iterate(State* state){
-    State end_ = N_TH(*state, 0) ^ (N_TH(*state, 31) | N_TH(*state, 30)) << 31;
-    *state = (*state & ~(1 << 31)) | (end_ << 31);
-    State n_state = (*state >> 1) ^ (*state | (*state << 1));
+    State end_ = N_TH(*state, 0) ^ (N_TH(*state, 63) | N_TH(*state, 62)) << 63;
+    *state = (*state & ~(1ULL << 63)) | (end_ << 63);
+    State n_state = (*state >> 1ULL) ^ (*state | (*state << 1ULL));
    
     // Hackish trick to prevent overflow of active cells
     // wrap around end cell
@@ -30,8 +30,8 @@ static inline void Iterate(State* state){
 }
 
 // Yield the center cell's current state
-static inline uint32_t Yield(State state){
-    return N_TH(state, 32/2); 
+static inline uint64_t Yield(State state){
+    return N_TH(state, 64/2); 
 }
 
 uint64_t Generate_64(State* rand, size_t bits){
@@ -46,7 +46,7 @@ uint64_t Generate_64(State* rand, size_t bits){
 }
 
 void PrintState(State state){
-    for (int i = 32; i--;){
+    for (int i = 64; i--;){
         const char cell = N_TH(state, i) ? '1' : '0';
         printf("%c", cell);
     }
@@ -54,14 +54,15 @@ void PrintState(State state){
 }
 
 int main(void){
-    assert(sizeof(State) == 4);
-    State rand = (uint32_t)time(NULL) * time(NULL) * time(NULL);
+    assert(sizeof(State) == 8);
+    State rand = (uint64_t)time(NULL) * time(NULL) * time(NULL);
 
     clock_t t;
     t = clock();
     for (int i = EPOCHS; --i;){
         uint64_t x = Generate_64(&rand, BITS);
-        // printf("%lu \n", x);
+        printf("%lu, ", x);
+        if (i+1 % 8== 0){ printf("\n"); }
     }
     t = clock() - t;
     double t_ = ((double)t/CLOCKS_PER_SEC);
